4ce528ed0f969672543d796233a1edcf2f390a8c
anhtayvc205 <anhtayvc205@gmail.com>
2026-01-30 03:30:19 +0700

    Create ShowItemMMOPlugin.java

---
+++ src/main/java/me/n/showitemmmo/ShowItemMMOPlugin.java
@@ -0,0 +1,15 @@
+package me.n.showitemmmo;
+
+import me.n.showitemmmo.listener.ChatListener;
+import me.n.showitemmmo.command.ShowItemCommand;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class ShowItemMMOPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        getCommand("showitem").setExecutor(new ShowItemCommand());
+        getServer().getPluginManager().registerEvents(new ChatListener(), this);
+        getLogger().info("ShowItemMMO enabled");
+    }
+}
